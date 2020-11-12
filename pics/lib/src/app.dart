a5f6a8ec9a49bac9cab8cdff9e1d69c0b990dc34
joshbrookstone <joshbrookstone@gmail.com>
2020-11-12 12:11:49 -0800

    courses added

---
+++ pics/lib/src/app.dart
@@ -0,0 +1,47 @@
+// IMport flutter helper lib
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+// Create a class that will be our custome widget
+// This class muist exten the 'StatelessWidget' base class
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter += 1;
+    final response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
+        appBar: AppBar(
+          title: Text("Lets see some images!"),
+        ),
+      ),
+    );
+  }
+}
+
+// Must define a 'build' method that reuns the widgeth that 'this' widghet will show
