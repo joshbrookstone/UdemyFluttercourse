3607a9033091b6e2e58c235dd0507bf38f49db4a
joshbrookstone <joshbrookstone@gmail.com>
2020-11-12 15:21:13 -0800

    added providers and streamers

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,3 +1,21 @@
 import 'dart:async';
+import 'validators.dart';
 
-class Bloc {}
+class Bloc extends Object with Validators {
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
+
+  //Add data to stream
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassowrd);
+
+  // Change data
+
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
+
+  dispose() {
+    _email.close();
+    _password.close();
+  }
+}
--- login_bloc/lib/src/screens/login_screen.dart
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import "../blocs/bloc.dart";
+import "../blocs/provider.dart";
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    final bloc = Provider.of(context);
+
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: [
-          emailField(),
-          passwordField(),
+          emailField(bloc),
+          passwordField(bloc),
           Container(
             margin: EdgeInsets.only(top: 25.0),
           ),
@@ -19,22 +22,36 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget emailField() {
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        hintText: 'you@example.com',
-        labelText: 'Email Address',
-      ),
+  Widget emailField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'you@example.com',
+            labelText: 'Email Address',
+            errorText: snapshot.error,
+          ),
+        );
+      },
     );
   }
 
-  Widget passwordField() {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: 'Password',
-        labelText: 'Password',
-      ),
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            labelText: 'Password',
+            errorText: snapshot.error,
+          ),
+        );
+      },
     );
   }
 
