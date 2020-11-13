5eab97ecc8a272bd494e99edae99fed6cab71b5c
joshbrookstone <joshbrookstone@gmail.com>
2020-11-13 09:49:07 -0800

    animations

---
+++ animation/lib/src/screens/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import "../widgets/cat.dart";
+
+class Home extends StatefulWidget {
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation'),
+      ),
+      body: buildAnimation(),
+    );
+  }
+
+  Widget buildAnimation() {
+    return Cat();
+  }
+}
